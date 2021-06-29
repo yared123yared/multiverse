7b2eb9eeff8f76177dacf746b20f76c5820b93ec
=yared123yared <=yaredyaya16@gmail.com>
2021-06-29 10:19:20 +0300

    updating the singIn

--- lib/app.dart
+++ lib/app.dart
@@ -18,7 +18,7 @@ class App extends StatelessWidget {
           ),
           home: Home(),
           onGenerateRoute: router.generateRoute,
-          initialRoute: router.SignInScreen),
+          initialRoute: router.HomeScreen),
     );
   }
 }
