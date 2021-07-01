43ed3211d3adc2967d08489dc05cf7b8ababb61a
=yared123yared <=yaredyaya16@gmail.com>
2021-07-01 15:21:27 +0300

    updating dashboard

---
+++ lib/widgets/Console/dashboard/Container_2/internetOfThings.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../button.dart';
+
+class InternetOfThingsButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Button(
+            elevation: 0,
+            icon: Icons.bar_chart,
+            value: 'Monitor',
+            size: 22,
+            onPressed: () {}));
+  }
+}
