2e69a886ff98a8608d121f5495132c0ba4c4f48e
=yared123yared <=yaredyaya16@gmail.com>
2021-07-03 08:59:18 +0300

    updated left container to be clickable

--- lib/widgets/Console/home/leftContainer/overview/container.dart
+++ lib/widgets/Console/home/leftContainer/overview/container.dart
@@ -7,10 +7,12 @@ class OverviewContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      // color: Color(0xff4B19DA),
-      // elevation: 1,
+    return InkWell(
       borderRadius: BorderRadius.circular(10),
+      hoverColor: Color(0xffE3F2FD).withOpacity(0.7),
+      highlightColor: Color(0xffE3F2FD).withOpacity(0.7),
+      splashColor: Color(0xffE3F2FD).withOpacity(0.7),
+      onTap: () {},
       child: Container(
           decoration: BoxDecoration(
               border: Border.all(color: Color(0xff4B19DA).withOpacity(0.2)),
